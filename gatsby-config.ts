456702585c3f2652b9849822eb517d8e10922cb0
junezhuo <yzzhuo2@gmail.com>
2024-03-11 22:53:34 +0200

    feat: blog mdx posts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -20,7 +20,8 @@ const config: GatsbyConfig = {
         name: `blog`,
         path: `${__dirname}/blog`,
       }
-    }
+    },
+    "gatsby-plugin-mdx"
   ]
 };
 
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -5,23 +5,29 @@ import { graphql } from 'gatsby'
 
 const BlogPage = ({data}: {
   data: {
-    allFile: {
+    allMdx: {
       nodes: {
-        name: string
+        frontmatter: {
+          date: string,
+          title: string
+        },
+        id: string,
+        excerpt: string
       }[]
     }
   }
-
-} ) => {
+}) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
         {
-          data.allFile.nodes.map(node => (
-            <li key={node.name}>{node.name}</li>
+          data.allMdx.nodes.map(node => (
+            <article key={node.id}>
+              <h2>{node.frontmatter.title}</h2>
+              <p>Posted: {node.frontmatter.date}</p>
+              <p>{node.excerpt}</p>
+            </article>
           ))
         }
-      </ul>
     </Layout>
   )
 }
@@ -32,9 +38,14 @@ export default BlogPage
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
       }
-  }
+    }
 }`
